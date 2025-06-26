fd748ad2f70600159555e076965535666bd83538
Benjamin CANTE <benjamin.cante@i-bp.fr>
2025-06-26 10:24:01 +0200

    Initial Commit

---
+++ WaterfallTT/WaterfallTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterfallTTApp.swift
+//  WaterfallTT
+//
+//  Created by CANTE Benjamin on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterfallTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
